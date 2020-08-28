3b18b9f92b3d3943e01e3e6109a70cd9cd9ca029
Andres Calle <andres.calle@trescloud.com>
2020-08-28 17:50:07 -0500

    [ADD] WIP of l10n_ec

---
+++ l10n_ec_edi/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import printer_point
+from . import res_company
+from . import res_users
+from . import payment_method
+from . import account_move
