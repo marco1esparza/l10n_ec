db4ce1f6648553d72dadafaf5d3da66f2964799d
Angel Parra <aparra@aitic.com.ar>
2020-09-03 12:45:25 -0500

    [FIX] Correccion en pre-migration.

--- l10n_ec/migrations/3.1/pre-migration.py
+++ l10n_ec/migrations/3.1/pre-migration.py
@@ -3,6 +3,7 @@
 
 def migrate(cr, version):
     l10n_ec_delete_view_accounts(cr)
+    l10n_ec_set_doc_type_no_update_false(cr)
 
 def l10n_ec_delete_view_accounts(cr):
     '''
@@ -13,3 +14,16 @@ def l10n_ec_delete_view_accounts(cr):
     cr.execute("DELETE FROM account_account WHERE user_type_id IN (SELECT id FROM account_account_type WHERE type = 'view')")
     cr.execute("DELETE FROM account_account_template")
     cr.execute("UPDATE ir_module_module SET state = 'to remove' where name = 'account_parent'")
+
+def l10n_ec_set_doc_type_no_update_false(cr):
+    '''
+    Setea el ir.model.data de los tipos de documentos a noupdate = False para poder actualizarlos.
+    '''
+    registry = odoo.registry(cr.dbname)
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    xml_record_ids = env['ir.model.data'].search([
+        ('model', 'in', ['l10n_latam.document.type']),
+        ('module', 'like', 'l10n_ec')
+    ]).ids
+    if xml_record_ids:
+        cr.execute("update ir_model_data set noupdate = 'f' where id in %s", (tuple(xml_record_ids),))
