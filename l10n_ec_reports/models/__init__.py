af270ae63dd2522bc83a19a43968dc0206af6f29
Angel Parra <arpr18@gmail.com>
2021-04-28 21:27:06 -0500

    Se agrega reporte A3, y se corrige A1 para que muestre valores en negativos.

--- l10n_ec_reports/models/__init__.py
+++ l10n_ec_reports/models/__init__.py
@@ -7,3 +7,4 @@ from . import res_partner
 from . import l10n_ec_account_tax_form_header
 from . import l10n_ec_a1_detail_report
 from . import l10n_ec_a2_detail_report
+from . import l10n_ec_a3_detail_report
