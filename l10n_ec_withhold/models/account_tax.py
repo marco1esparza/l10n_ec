82cb003e48371a8b8bc47e55409cf34106747e5f
Andres Calle <andres.calle@trescloud.com>
2021-06-01 12:19:26 -0500

    [FIX] Singleton

--- l10n_ec_withhold/models/account_tax.py
+++ l10n_ec_withhold/models/account_tax.py
@@ -11,16 +11,17 @@ class AccountTax(models.Model):
         # - alter base amount for vat withhold after computation, to store the correct base amount
         # - compute vat withold as globally rounding
         res = super(AccountTax, self).compute_all(price_unit, currency, quantity, product, partner, is_refund, handle_price_include)
-        withhold = self.filtered(lambda t: t.tax_group_id.l10n_ec_type in ('withhold_vat', 'withhold_income_tax'))
-        if withhold:
+        withholds = self.filtered(lambda t: t.tax_group_id.l10n_ec_type in ('withhold_vat', 'withhold_income_tax'))
+        if withholds:
             for tax in res['taxes']:
-                if tax['id'] == withhold.id:
+                if tax['id'] in withholds.ids:
                     sign = 1
                     base = tax['base']
                     if currency.is_zero(base):
                         sign = self._context.get('force_sign', 1)
                     elif base < 0:
                         sign = -1
+                    withhold = withholds.filtered(lambda t: t.id == tax['id'])
                     tax_amount = withhold.with_context(force_price_include=False)._compute_amount(tax['base'], sign * price_unit, quantity, product, partner)
                     prec = currency.rounding * 1e-5
                     tax['amount'] = float_round(tax_amount, precision_rounding=prec)
