6182eda3d636c977ad44f0d107f7f30f29585201
Andres Calle <andres.calle@trescloud.com>
2021-06-29 10:35:13 -0500

    [FIX] Ingreso a menu de cuentas contables

--- l10n_ec/migrations/1.2/post-migration.py
+++ l10n_ec/migrations/1.2/post-migration.py
@@ -1,23 +1,16 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-# import odoo
-# from odoo import api, SUPERUSER_ID
-# from openupgradelib import openupgrade
-# 
-# @openupgrade.migrate()
-# def migrate(env, version):
-#     rename_salary_journal(env)
-# 
-# @openupgrade.logging()
-# def rename_salary_journal(env):
-#     companies = env['res.company'].search([])
-#     for company in companies:
-#         salary_journal = env['account.journal'].search([
-#                 ('code', '=', 'SALAR'),
-#                 ('company_id', '=', company.id)])
-#         salary_journal.write({
-#             'name': 'Rol de Pagos',
-#             'code': 'ROLDP',
-#             })
-    
+import odoo
+from odoo import api, SUPERUSER_ID
+from openupgradelib import openupgrade
+ 
+@openupgrade.migrate()
+def migrate(env, version):
+    recompute_account_root(env)
+ 
+@openupgrade.logging()
+def recompute_account_root(env):
+    # re-computes the first 2 digits of the account.
+    accounts = env['account.account'].search([])
+    accounts._compute_account_root()
