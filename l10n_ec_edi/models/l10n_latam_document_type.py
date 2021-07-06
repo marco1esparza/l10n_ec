bd5a135f308249604e52192d96ec2a0c284406f7
Andres Calle <andres.calle@trescloud.com>
2021-07-05 22:55:23 -0500

    Removido metodo innecesario

--- l10n_ec_edi/models/l10n_latam_document_type.py
+++ l10n_ec_edi/models/l10n_latam_document_type.py
@@ -6,20 +6,7 @@ from odoo.osv import expression
 
 class L10nLatamDocumentType(models.Model):
     _inherit = 'l10n_latam.document.type'
-
-    @api.model
-    def name_search(self, name='', args=None, operator='ilike', limit=100):
-        '''
-        Se modifica el name_search para que se buscar el Codigo del document type
-        '''
-        args = args or []
-        if self.env.company.country_id != self.env.ref('base.ec'):
-            return super().name_search(name, args, operator, limit)
-        else:
-            domain = [('active', 'ilike', True), '|', ('code', 'ilike', name), ('name', operator, name)]
-        doc_types = self.search(expression.AND([domain, args]), limit=limit)
-        return doc_types.name_get()
-
+    
     def l10n_ec_map_document_type_code_to_electronic_code(self):
         # Retorna el codigo de documento electronico para la clave de acceso
         # TODO V15, usar este metodo en _l10n_ec_set_access_key()
