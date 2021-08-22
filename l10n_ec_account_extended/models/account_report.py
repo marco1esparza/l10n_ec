38c5a40e47a74e3e8ba6d685f3b3d7d0a0dded78
Angel Parra <arpr18@gmail.com>
2021-08-22 16:49:25 -0500

    [Fix] Limpieza de codigo

--- l10n_ec_account_extended/models/account_report.py
+++ l10n_ec_account_extended/models/account_report.py
@@ -1,25 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-import ast
-import copy
-import json
-import io
 import logging
-import lxml.html
-import datetime
-import ast
-from collections import defaultdict
-from math import copysign
-
-from dateutil.relativedelta import relativedelta
-
-from odoo.tools.misc import xlsxwriter
-from odoo import models, fields, api, _
-from odoo.tools import config, date_utils, get_lang
-from odoo.osv import expression
-from babel.dates import get_quarter_names
-from odoo.tools.misc import formatLang, format_date
-from odoo.addons.web.controllers.main import clean_action
+from odoo import models
 
 _logger = logging.getLogger(__name__)
 
