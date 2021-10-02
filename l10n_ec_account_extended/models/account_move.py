ba7ea442372d7aa650b5b26996ead9dcec875570
Angel Parra <arpr18@gmail.com>
2021-10-02 15:55:31 -0500

    [ADD] Se coloca validacion para permitir solo 300 caracteres al campo terminos y condiciones.

--- l10n_ec_account_extended/models/account_move.py
+++ l10n_ec_account_extended/models/account_move.py
@@ -163,7 +163,9 @@ class AccountMove(models.Model):
                 ecuador_edis = document.journal_id.edi_format_ids.filtered(lambda x: x.code == 'l10n_ec_tax_authority')
                 unnecesary_edis = document.journal_id.edi_format_ids - ecuador_edis
                 document.journal_id.sudo().edi_format_ids -= unnecesary_edis #se elimina el factur-x del diarios
-
+                narration = len(document.narration)
+                if narration > 300:
+                    raise UserError(_("Por favor, Corrija los términos y cóndiciones pues es muy extenso, se aceptan máximo 300 caracteres y usted ingresó %s caracteres") % narration)
                 if document.journal_id.compatible_edi_ids.filtered(lambda e: e.code == 'facturx_1_0_05'):
                     raise UserError(_("Por favor, debe deshabilitar primero el Documento Electrónico Factur-X (FR) del Diario %s, Contabilidad/Configuración/Diarios Contables") % self.journal_id.name)
                 #FIX ME: a veces, con puntos de emision nuevos, no se computa el perfijo de la factura en el numero
