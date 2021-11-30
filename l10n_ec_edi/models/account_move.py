a581d07acbcd476f3a74433311f0420ca212aaa0
jmrivero <jose.rivero@trescloud.com>
2021-11-29 20:35:05 -0500

    Terminando de mover code

--- l10n_ec_edi/models/account_move.py
+++ l10n_ec_edi/models/account_move.py
@@ -555,59 +555,6 @@ class AccountMove(models.Model):
             res.show_l10n_ec_authorization = show_l10n_ec_authorization
             res.edit_l10n_ec_authorization = edit_l10n_ec_authorization
 
-    @api.depends('l10n_ec_custom_line_ids.price_subtotal', 'partner_id', 'currency_id')
-    def _compute_invoice_custom_taxes_by_group(self):
-        for move in self.filtered('l10n_latam_document_type_id'):
-            if not move.is_invoice(include_receipts=True) or move.move_type not in ('out_invoice', 'out_refund') \
-                    or not move.l10n_ec_invoice_custom:
-                move.amount_custom_by_group = {}
-                continue
-
-            is_refund = False
-            handle_price_include = False
-            if move.is_invoice(include_receipts=True):
-                is_refund = move.move_type in ('out_refund', 'in_refund')
-                handle_price_include = True
-
-            lang_env = move.with_context(lang=move.partner_id.lang).env
-            tax_balance_multiplicator = 1 if move.is_inbound(True) else -1
-            res = {}
-            for line in move.l10n_ec_custom_line_ids:
-                for tax in line.tax_ids.flatten_taxes_hierarchy():
-                    if tax.tax_group_id not in res:
-                        res.setdefault(tax.tax_group_id, {'base': 0.0, 'amount': 0.0})
-
-                    base_amount = tax_balance_multiplicator * (line.price_subtotal)
-                    res[tax.tax_group_id]['base'] += base_amount
-                    tax_amount = tax.with_context(
-                        force_sign=move._get_tax_force_sign()).compute_all(
-                        base_amount,
-                        currency=line.currency_id,
-                        quantity=1.0,
-                        product=False,
-                        partner=line.partner_id,
-                        is_refund=is_refund,
-                        handle_price_include=handle_price_include,
-                    )
-                    res[tax.tax_group_id]['amount'] += tax_amount['taxes'][0]['amount']
-
-            res = sorted(res.items(), key=lambda l: l[0].sequence)
-            move.amount_custom_by_group = [(
-                group.name, amounts['amount'],
-                amounts['base'],
-                formatLang(lang_env, amounts['amount'], currency_obj=move.currency_id),
-                formatLang(lang_env, amounts['base'], currency_obj=move.currency_id),
-                len(res),
-                group.id
-            ) for group, amounts in res]
-
-    amount_custom_by_group = fields.Binary(string="Tax amount by group",
-                                    compute='_compute_invoice_custom_taxes_by_group',
-                                    help='Edit Tax amounts if you encounter rounding issues.')
-    l10n_ec_invoice_custom = fields.Boolean(string='Factura Personalizada')
-    l10n_ec_custom_line_ids = fields.One2many('l10n_ec.custom.move.line', 'move_id', string='Invoice lines',
-                                       copy=False, readonly=True,
-                                       states={'draft': [('readonly', False)]})
     l10n_ec_printer_id = fields.Many2one(
         'l10n_ec.sri.printer.point',
         string='Punto de emisión', readonly = True,
