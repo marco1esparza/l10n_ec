2f1f8ff31f97fe5c32f26f062361c295210ddafc
Andres Calle <andres.calle@trescloud.com>
2022-08-08 00:02:06 -0500

    [DEL] Deleted methods moved to official version

--- l10n_ec_account_extended/models/account_journal.py
+++ l10n_ec_account_extended/models/account_journal.py
@@ -4,7 +4,6 @@ from odoo.exceptions import ValidationError
 
 
 class AccountJournal(models.Model):
-
     _inherit = "account.journal"
 
     @api.constrains('l10n_ec_entity', 'l10n_ec_emission')
@@ -21,13 +20,3 @@ class AccountJournal(models.Model):
                 raise ValidationError(_(
                     'You can not modify the "Withhold Type" if there are validated withholds in this journal!'))
 
-    def _l10n_ec_require_emission(self):
-        #Used in account.move to validate the entity and emission point matches the invoice number
-        if self.country_code == 'EC' and self.l10n_latam_use_documents:
-            if self.type == 'sale':
-                return True
-            elif self.type == 'purchase' and self.l10n_ec_is_purchase_liquidation:
-                return True
-            elif self.l10n_ec_withhold_type == 'in_withhold':
-                return True
-        return False
